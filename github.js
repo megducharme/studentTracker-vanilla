a0449e7e97a49208c571a7cf585e776726e4a859
Meg Ducharme <ducharme.meg@gmail.com>
2018-02-12 18:39:46 -0600

    updated function call

--- github.js
+++ github.js
@@ -5,54 +5,58 @@ $(document).ready(function () {
 
     document.getElementById("c22").addEventListener("click", function(){
         cohort = "./c22.json"
+        getStudentData()
     })
 
     document.getElementById("c23").addEventListener("click", function(){
         cohort = "./c23.json"
+        getStudentData()
     })
     
-    $.ajax({
-        type: "GET",
-        url: cohort
-    })
-    .then(data => {
-        students = data
-        return students
-    })
-    .then(students => {
-        students.forEach(currentStudent => {
-            $.ajax({
-                type: "GET",
-                url: `https://spyproxy.bangazon.com/student/commit/https://api.github.com/users/${currentStudent.githubHandle}/events`,
-                success: function (data) {
-                    console.log(data)
-                    let lastCommit = data.find(event => {
-                        return event.type === "PushEvent"
-                    })
-                    console.log(lastCommit)
+    function getStudentData(){
+        $.ajax({
+            type: "GET",
+            url: cohort
+        })
+        .then(data => {
+            students = data
+            return students
+        })
+        .then(students => {
+            students.forEach(currentStudent => {
+                $.ajax({
+                    type: "GET",
+                    url: `https://spyproxy.bangazon.com/student/commit/https://api.github.com/users/${currentStudent.githubHandle}/events`,
+                    success: function (data) {
+                        console.log(data)
+                        let lastCommit = data.find(event => {
+                            return event.type === "PushEvent"
+                        })
+                        console.log(lastCommit)
 
-                    let studentData = {}
-                    studentData.student = currentStudent
-    
-                    let lastPush = new Date(lastCommit.created_at)
-                    let today = new Date(Date.now())
-                    studentData.diffDays = parseInt((today - lastPush) / (1000 * 60 * 60 * 24)) + " days ago"
-    
-                    studentData.repo = lastCommit.repo.name.split("/")[1]
-                    studentData.message = lastCommit.payload.commits[0].message
-    
-                    studentData.color = "red"
-    
-                    if (studentData.diffDays === 0 + " days ago") {
-                        studentData.diffDays = "today"
-                        studentData.color = "green"
+                        let studentData = {}
+                        studentData.student = currentStudent
+        
+                        let lastPush = new Date(lastCommit.created_at)
+                        let today = new Date(Date.now())
+                        studentData.diffDays = parseInt((today - lastPush) / (1000 * 60 * 60 * 24)) + " days ago"
+        
+                        studentData.repo = lastCommit.repo.name.split("/")[1]
+                        studentData.message = lastCommit.payload.commits[0].message
+        
+                        studentData.color = "red"
+        
+                        if (studentData.diffDays === 0 + " days ago") {
+                            studentData.diffDays = "today"
+                            studentData.color = "green"
+                        }
+                        
+                        printToDOM(studentData)
                     }
-                    
-                    printToDOM(studentData)
-                }
+                })
             })
         })
-    })
+    }
 
     function printToDOM(student){
 
