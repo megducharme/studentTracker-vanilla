cfcae3ca8f09f8a40328132a384eebe995db99b8
Meg Ducharme <ducharme.meg@gmail.com>
2018-03-01 11:38:44 -0600

    refactoring

--- github.js
+++ github.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
         let jsonAddress = event.target.id
         if(jsonAddress.startsWith("c__")){
             $("#output").html("")
-            let cohort = `./${jsonAddress.split("__")[1]}.json`
+            let cohort = `./c${jsonAddress.split("__")[1]}.json`
             getStudentData(cohort)
         }
     })
