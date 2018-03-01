8cdd99c366f0ac42f48c62fc619c04299c227613
Meg Ducharme <ducharme.meg@gmail.com>
2018-02-28 18:35:45 -0600

    fork update

--- github.js
+++ github.js
@@ -86,9 +86,11 @@ $(document).ready(function () {
 
 
                         if (data[0].type === "ForkEvent") {
+                            let forkDate = new Date(data[0].payload.pushed_at.created_at)
+                            studentData.diffDays = parseInt((today - forkDate) / (1000 * 60 * 60 * 24)) + " days ago"
                             studentData.event = "fork"
                             studentData.repo = data[0].repo.name.split("/")[1]
-                            studentData.message = "-"
+                            studentData.message = `-`
                         }
 
                         allStudents.push(studentData)
